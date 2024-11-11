83b2416435fd8549ff0f7d3458560d367dd66ba5
lautiod <dantelojeda05@gmail.com>
2024-11-11 03:28:10 -0300

    Docker del Front + Coors de courses-api

---
+++ frontend/src/components/Course.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import './styles/Course.css';
+
+function Course({ course }) {
+    // const handleEnroll = async () => {
+    //     try {
+    //         const response = await fetch('http://localhost:8080/subscribe', {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify({ courseID: course.courseID}), // Adjust this based on how you identify the user
+    //             credentials: 'include'
+    //         });
+
+    //         if (response.ok) {
+    //             alert('Inscripción exitosa!');
+    //         } else {
+    //             alert('Error al inscribirse en el curso.');
+    //         }
+    //     } catch (error) {
+    //         console.error('Error:', error);
+    //         alert('Error al inscribirse en el curso.');
+    //     }
+    // };
+
+    return (
+        <div className="course-card">
+            <div className="course-content">
+                <h2>{course.name}</h2>
+                <p>Descripcion: {course.description}</p>
+                {/* <Link to={`/course/${course.courseID}`}>
+                    <button className="info-button">Información</button>
+                </Link>
+                <button className="enroll-button" onClick={handleEnroll}>Inscribirse</button> */}
+            </div>
+        </div>
+    );
+}
+
+export default Course;
